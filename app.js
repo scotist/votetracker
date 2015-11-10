65922d360c3a3107d98ab7329ae5bfb09f7bec55
Michael Sullivan <sapientia.veritas@gmail.com>
2015-11-10 13:56:33 -0800

    added chart.js, minor edits to app.js, minor additions to index.html

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 var images = [];
+var picture1 = document.getElementById('img1');
+var picture2 = document.getElementById('img2');
 
 function failCakes(name, path) {
   this.name = name;
   this.path = path;
   this.votes = 0;
   images.push(this);
+  // tracker.images.push(this);
 };
 
 var baby = new failCakes("baby", src="images/baby.png");
@@ -29,11 +32,11 @@ var random = function () {
 random();
 
 
-calcNewIndex = function (index1) {
+calcNewNum = function (num1) {
   do {
-    var index2 = random();
-  } while (index1 === index2)
-  return index2;
+    var num2 = random();
+  } while (num1 === num2)
+  return num2;
 }
 
 vote = function (index) {
@@ -45,12 +48,11 @@ vote = function (index) {
   displayImages();
 }
 
-var picture1 = document.getElementById('img1');
-var picture2 = document.getElementById('img2');
+
 
 displayImages = function () {
-  var index1 = random();
-  var index2 = calcNewIndex(index1);
+  var num1 = random();
+  var num2 = calcNewNum(num1);
   var temp = document.getElementById('temp');
 
 
@@ -59,24 +61,88 @@ displayImages = function () {
   picture1.setAttribute('id', 'img1');
   picture2.setAttribute('id', 'img2');
 
-  picture1.setAttribute('src', images[index1].path);
-  picture2.setAttribute('src', images[index2].path);
+  picture1.setAttribute('src', images[num1].path);
+  picture2.setAttribute('src', images[num2].path);
 
   picture1.setAttribute('width', '300');
   picture2.setAttribute('width', '300');
   temp.appendChild(picture1);
   temp.appendChild(picture2);
 
+
   picture1.addEventListener('click', function(){
-    vote(index1) //  a random number
+    vote(num1);
+    console.log('You voted for ' + images[num1].name);
   });
 
   picture2.addEventListener('click', function(){
-    vote(index2)
+    vote(num2);
+    console.log('You voted for ' + images[num2].name);
     });
 
 }
 
 displayImages();
+
+// var tracker = {
+//   images: []
+// };
+
+// tracker.random = function () {
+//   return Math.floor(Math.random() * images.length);
+//   console.log(random);
+// };
+
+// tracker.calcNewNum = function (num1) {
+//   do {
+//     var num2 = tracker.random();
+//   } while (num1 === num2)
+//   return num2;
+// };
+
+// tracker.vote = function (index) {
+//   console.log(images[index].name + ' had ' + images[index].votes + ' votes.');
+//   images[index].votes++;
+//   console.log(images[index].name + ' now has ' + images[index].votes + ' votes.');
+//   picture1.parentNode.removeChild(picture1);
+//   picture2.parentNode.removeChild(picture2);
+//   displayImages();
+// };
+
+// tracker.displayImages = function () {
+//   var num1 = tracker.random();
+//   var num2 = tracker.calcNewNum(num1);
+//   var temp = document.getElementById('temp');
+
+
+//   picture1 = document.createElement('img');
+//   picture2 = document.createElement('img');
+//   picture1.setAttribute('id', 'img1');
+//   picture2.setAttribute('id', 'img2');
+
+//   picture1.setAttribute('src', images[num1].path);
+//   picture2.setAttribute('src', images[num2].path);
+
+//   picture1.setAttribute('width', '300');
+//   picture2.setAttribute('width', '300');
+//   temp.appendChild(picture1);
+//   temp.appendChild(picture2);
+
+//   picture1.addEventListener('click', function(){
+//     vote(num1);
+//     console.log('You voted for ' + images[num1].name);
+//   });
+
+//   picture2.addEventListener('click', function(){
+//     vote(num2);
+//     console.log('You voted for ' + images[num2].name);
+//     });
+
+// };
+
+// tracker.random();
+// tracker.calcNewNum();
+// tracker.vote();
+// tracker.displayImages();
 
 
