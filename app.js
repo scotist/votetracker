44074374ba45e66d0adef8cd286e850f9cfd4360
Michael Sullivan <sapientia.veritas@gmail.com>
2015-11-10 11:49:17 -0800

    minor cleanups and edits

--- app.js
+++ app.js
@@ -7,14 +7,6 @@ function failCakes(name, path) {
   images.push(this);
 };
 
-var random = function () {
-  return Math.floor(Math.random() * images.length);
-  console.log(random);
-}
-
-random();
-
-
 var baby = new failCakes("baby", src="images/baby.png");
 var buzz = new failCakes("buzz", src="images/buzz.png");
 var clown = new failCakes("clown", src="images/clown.png");
@@ -29,6 +21,14 @@ var shoes = new failCakes("shoes", src="images/shoes.png");
 var sprinkles = new failCakes("sprinkles", src="images/sprinkles.png");
 var toenail = new failCakes("toenail", src="images/toenail.png");
 
+var random = function () {
+  return Math.floor(Math.random() * images.length);
+  console.log(random);
+}
+
+random();
+
+
 calcNewIndex = function (index1) {
   do {
     var index2 = random();
@@ -42,39 +42,41 @@ vote = function (index) {
   console.log(images[index].name + ' now has ' + images[index].votes + ' votes.');
   picture1.parentNode.removeChild(picture1);
   picture2.parentNode.removeChild(picture2);
-  displayPhotos();
+  displayImages();
 }
 
 var picture1 = document.getElementById('img1');
 var picture2 = document.getElementById('img2');
 
-displayPhotos = function () {
+displayImages = function () {
   var index1 = random();
   var index2 = calcNewIndex(index1);
   var temp = document.getElementById('temp');
 
 
-picture1 = document.createElement('img');
-picture2 = document.createElement('img');
-picture1.setAttribute('id', 'img1');
-picture2.setAttribute('id', 'img2');
+  picture1 = document.createElement('img');
+  picture2 = document.createElement('img');
+  picture1.setAttribute('id', 'img1');
+  picture2.setAttribute('id', 'img2');
 
-picture2.setAttribute('src', images[index2].path);
+  picture1.setAttribute('src', images[index1].path);
+  picture2.setAttribute('src', images[index2].path);
 
-picture1.setAttribute('src', images[index1].path);
-picture1.setAttribute('width', '300');
-picture2.setAttribute('width', '300');
-temp.appendChild(picture1);
-temp.appendChild(picture2);
+  picture1.setAttribute('width', '300');
+  picture2.setAttribute('width', '300');
+  temp.appendChild(picture1);
+  temp.appendChild(picture2);
 
-picture1.addEventListener('click', function(){
-  vote(index1) //  a random number
-});
-picture2.addEventListener('click', function(){
-  vote(index2)
+  picture1.addEventListener('click', function(){
+    vote(index1) //  a random number
   });
 
+  picture2.addEventListener('click', function(){
+    vote(index2)
+    });
+
 }
-displayPhotos();
+
+displayImages();
 
 
